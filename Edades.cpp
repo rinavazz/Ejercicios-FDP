a3fc8f3fd0110f00930544f21c9b45f9c8d2a59c
rinavazz <00167525@uca.edu.sv>
2025-05-06 10:08:20 -0600

    Ejercicio 1

---
+++ Edades.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+
+    int n; 
+    cout << "Ingresa tu edad:\n";
+    cin >> n; 
+
+    if (n>18){
+        cout << "Eres mayor de edad\n";
+    }else{
+        if(n<18){
+            cout << "Eres menor edad\n";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
