0a761c5f6bd19d5173f34d50622872a78a82b563
melanievmz <00185125@uca.edu.sv>
2025-05-10 00:33:09 -0600

    agrego mi ejercicio

---
+++ ejercicio_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int numero;
+    cout << "ingrese el número: ";
+    cin >> numero;
+
+    if(numero % 2 == 0){
+        cout << "el número es par" << endl;
+    
+    } else {
+        cout << " el n´´umero es impar" << endl;
+    
+    } 
+
+    return 0;
+
+
+}
